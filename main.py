d072653f29ec06dcc6aaa2737751a718a9a0bfa5
Pedro Labrador <pedrolabrador3@outlook.com>
2019-12-29 11:49:26 -0400

    ad favorite anime to the list

--- main.py
+++ main.py
@@ -13,61 +13,89 @@ import os
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-def add_favorite_anime():
-	pass
+def add_favorite_anime(anime_name):
+	home = os.path.expanduser('~')
+
+	if sys.platform in ('linux', 'linux2'):
+		folder_path = home + '/.animeflv'
+		list_path   = folder_path + '/favs.dat'
+
+		if not os.path.exists(folder_path):
+			os.mkdir(folder_path)
+		else:
+			if os.path.exists(list_path):
+				anime = get_anime_url(anime_name, episode_list=False)
+				if anime is not None:
+					with open(list_path, 'r') as anime_file:
+						anime_list = [item.replace('\n', '').split(',') for item in anime_file.readlines()]
+						anime_list = list(filter(lambda x: x[1] == anime['slug'], anime_list))
+						if len(anime_list) > 0:
+							sys.exit("Anime already exists")
+					with open(list_path, 'a+') as anime_list:
+						anime_path = f"{folder_path}/{anime['id']}.dat"
+						anime_list.write(f"{anime['id']},{anime['slug']}\n")
+						open(anime_path, 'a').close()
+					sys.exit(f"{anime['title']} added to your favorite list")
+				else:
+					print("Error - Anime returned None")
+
+	sys.exit(0)
+
 
 def check_updates():
 	home = os.path.expanduser('~')
 	
 	if sys.platform in ('linux', 'linux2'):
 		folder_path = home + '/.animeflv'
-		list_path   = folder_path + '/list.dat'
+		list_path   = folder_path + '/favs.dat'
 
 		if not os.path.exists(folder_path):
 			os.mkdir(folder_path)
 		else:
-			list_file = open(list_path, 'r')
-			for item in list_file.readlines():
-				if not item.startswith('#'):
-					data  = item.replace('\n','').split(',')
-					url   = f"https://animeflv.net/anime/{data[0]}/{data[1]}"
-					anime = { 'url': url, 'id': data[0], 'name': data[1] }
-					episodes = get_episode_list(anime, updates=True)
-					episodes = [",".join([str(it_episode[0]), str(it_episode[1])]) for it_episode in episodes]
-					anime_path = f"{folder_path}/{data[0]}.dat"
-					not_watched_list = []
+			if os.path.exists(list_path):
+				with open(list_path, 'r') as list_file:
+					for item in list_file.readlines():
+						if not item.startswith('#'):
+							data  = item.replace('\n','').split(',')
+							url   = f"https://animeflv.net/anime/{data[0]}/{data[1]}"
+							anime = { 'url': url, 'id': data[0], 'name': data[1] }
+							episodes = get_episode_list(anime, updates=True)
+							episodes = [",".join([str(it_episode[0]), str(it_episode[1])]) for it_episode in episodes]
+							anime_path = f"{folder_path}/{data[0]}.dat"
+							not_watched_list = []
 
-					if os.path.exists(anime_path):
-						episode_file = open(anime_path, 'r')
-						episode_list = [episode.replace('\n', '') for episode in episode_file.readlines()]
-						episode_file.close()
+							if os.path.exists(anime_path):
+								with open(anime_path, 'r') as episode_file:
+									episode_list = [episode.replace('\n', '') for episode in episode_file.readlines()]
 
-						for episode in episodes:
-							if not episode in episode_list:
-								not_watched_list.append(episode)
+								for episode in episodes:
+									if not episode in episode_list:
+										not_watched_list.append(episode)
 
-						for not_watched in not_watched_list:
-							episode = not_watched.split(',')
-							id = episode[1]
-							slug = anime['name'] + '-' + str(episode[0])
+								for not_watched in not_watched_list:
+									episode = not_watched.split(',')
+									id = episode[1]
+									slug = anime['name'] + '-' + str(episode[0])
 
-							print(f"Not watched episode #{episode[0]}")
+									print(f"Not watched episode #{episode[0]}")
 
-							episode_url = f"https://animeflv.net/ver/{id}/{slug}"
-							get_episode_download_link(episode_url, all_links=True)
-						
-							with open(anime_path, 'a+') as episode_file:
-								episode_file.write(not_watched)
+									episode_url = f"https://animeflv.net/ver/{id}/{slug}"
+									get_episode_download_link(episode_url, all_links=True)
+								
+									with open(anime_path, 'a+') as episode_file:
+										episode_file.write(not_watched + '\n')
 
-						if len(not_watched_list) == 0:
-							print("No new episodes")
+								if len(not_watched_list) == 0:
+									print("No new episodes")
+			else:
+				print("No anime list found")
 			
 	if sys.platform in ('win32'):
 		pass
 
 	sys.exit(0)
 
-def get_anime_url(anime_name):
+def get_anime_url(anime_name, episode_list=True):
 	params = {'value': anime_name}
 	animeflv_search = "https://animeflv.net/api/animes/search"
 	anime = None
@@ -100,7 +128,12 @@ def get_anime_url(anime_name):
 		print(f'Other error occurred: {err}')
 
 	if anime:
-		get_episode_list(anime)
+		if episode_list:
+			get_episode_list(anime)
+		else:
+			return anime
+	else:
+		return None
 
 
 def get_episode_list(anime, default_download='mega', updates=False):
@@ -212,6 +245,7 @@ parser = OptionParser(option_class=URL)
 parser.add_option("-u", "--url", dest="url", help="decode URL", type="url")
 parser.add_option("-r", "--request", dest="get_anime_url", help="scrape for specific anime URLs")
 parser.add_option("-l", "--limit", help="limit default=10")
+parser.add_option("--addfavorite", dest="add_favorite_anime", help="Add your favorite anime to the list")
 parser.add_option("-c", "--checkupdates", action="store_true", dest="checkupdates", help="Checks if theres a new episode of your favorite list, needs to add favorites first.")
 parser.add_option("--range", help="range example=1,10")
 parser.add_option("-m", "--mega", action="store_true", help="download link default mega")
@@ -223,6 +257,8 @@ parser.add_option("-a", "--all", action="store_true", help="download all links")
 if __name__ == '__main__':
 	if options.checkupdates:
 		check_updates()
+	if options.add_favorite_anime:
+		add_favorite_anime(options.add_favorite_anime)
 	if options.url:
 		url = options.url
 		url = urllib.parse.unquote(url)
